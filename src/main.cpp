72e36b4ec2c8b43a9b371680b7ca657f534c6b7d
AndriiDvornytskyi <155467329+AndriiDvornytskyi@users.noreply.github.com>
2025-01-17 16:00:24 +0200

    Перша версія коду

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,62 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <PID_v1.h>
+#include <Adafruit_MAX31865.h>
 
+#define RELAY_PIN 7  // вивід для підключення регулюючого транзистора
 
+#define RREF 430.0
+#define RNOMINAL 100.0
+Adafruit_MAX31865 thermo = Adafruit_MAX31865(10, 11, 12, 13);  //підключення перетвотювача датчика Pt100
 
-// put function declarations here:
-int myFunction(int, int);
+// Змінні для ПІД-регулятора
+double Setpoint, Input, Output;
+double Kp = 2.5, Ki = 0.5, Kd = 1.5;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  thermo.begin(MAX31865_4WIRE);  // 4 провідне підключення Pt100
+  Setpoint = 100.0;  // Задана температура
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(5, OUTPUT);
+  myPID.SetMode(AUTOMATIC);
+  myPID.SetOutputLimits(0, 255);  // Вихід від 0 до 255 (ШІМ)
+  myPID.SetSampleTime(100);       // Встановлення періоду опитування ПІД-регулятора на 100 мілісекунд (0.1 секунди)
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  uint16_t rtd = thermo.readRTD(); // Отримання поточного опору RTD
+  uint8_t status = thermo.readFault(); // Перевірка статусу помилок
+  if (status) {
+    Serial.print("Fault: ");
+    if (status & MAX31865_FAULT_HIGHTHRESH) Serial.println("RTD High Threshold");
+    if (status & MAX31865_FAULT_LOWTHRESH) Serial.println("RTD Low Threshold");
+    if (status & MAX31865_FAULT_REFINLOW) Serial.println("REFIN- > 0.85 x Bias");
+    if (status & MAX31865_FAULT_REFINHIGH) Serial.println("REFIN- < 0.85 x Bias - FORCE- open");
+    if (status & MAX31865_FAULT_RTDINLOW) Serial.println("RTDIN- < 0.85 x Bias - FORCE- open");
+    if (status & MAX31865_FAULT_OVUV) Serial.println("Under/Over voltage");
+    thermo.clearFault();
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Виведення значення опору
+  Serial.print("RTD value: ");
+  Serial.println(rtd);
+
+  // Отримання поточної температури
+  Input = thermo.temperature(RNOMINAL, RREF);
+  digitalWrite(5, HIGH);
+  // Виклик функції обчислення ПІД-регулятора
+  myPID.Compute();
+
+  // Застосування виходу ПІД-регулятора до виконавчого механізму
+  analogWrite(RELAY_PIN, Output);
+
+  // Виведення значень для налагодження
+  Serial.print("Temperature: ");
+  Serial.print(Input);
+  Serial.print(" C  Output: ");
+  Serial.println(Output);
+
+  delay(100);  // Затримка для стабільного читання датчика
 }
